e33793bc34c371a33caf6801d623501e814a2f84
KassemHachi <kassem.hachi@outlook.com>
2023-12-17 21:23:44 +0100

    add  chevron left heart and retweet icons

---
+++ resources/views/components/icons/chevron-left.blade.php
@@ -0,0 +1,4 @@
+<svg {{$attributes}} width="10" height="19" viewBox="0 0 10 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M8.73315 17.1853L2.20833 10.6605C1.43777 9.88991 1.43777 8.62898 2.20833 7.85841L8.73315 1.33359"
+        stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
