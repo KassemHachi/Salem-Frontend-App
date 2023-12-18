c90b425e74bdd84be51529d8a137a6c292c4d09e
KassemHachi <kassem.hachi@outlook.com>
2023-12-18 17:16:29 +0100

    add icons and patterns element

---
+++ resources/views/components/icons/chevron-right.blade.php
@@ -0,0 +1,4 @@
+<svg {{$attributes}} width="10" height="19" viewBox="0 0 13 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.79981 23.9758L10.9278 14.8478C12.0058 13.7698 12.0058 12.0058 10.9278 10.9278L1.7998 1.79984"
+        stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
