197915454337efce3fd41880a93b37943b3afa50
KassemHachi <kassem.hachi@outlook.com>
2023-12-19 21:51:00 +0100

    fix responsive footer

---
+++ resources/views/components/commun/footer.blade.php
@@ -0,0 +1,80 @@
+<footer class="relative z-0 px-20 py-10 overflow-hidden bg-salem-dark-blue-500">
+    <div class="flex flex-col gap-y-10 gap-x-20 lg:flex-row pt-28">
+        <div class="w-full">
+            <x-commun.logo class="h-20 mb-2 fill-white" />
+            <p class="font-normal text-white">
+                رجل الاعمال / الاستاذ سالم بن علي بن بغيّه العجمي من مواليد مدينة نجران عام 1972 م بالمملكة العربية
+                السعودية
+            </p>
+        </div>
+        <div class="flex flex-col justify-between w-full gap-4">
+            <h3 class="text-lg font-medium text-white">
+                روابط سريعة
+            </h3>
+            <ul class="grid grid-cols-2 gap-2 text-white lg:grid-cols-4 lg:gap-3">
+                <a href="#">
+                    <li>الرئيسية</li>
+                </a>
+                <a href="#">
+                    <li>سالم عن قرب</li>
+                </a>
+                <a href="#">
+                    <li>الأعمال والإنجازات</li>
+                </a>
+                <a href="#">
+                    <li>البرنامج الإنتخابي</li>
+                </a>
+                <a href="#">
+                    <li>تواصل معنا</li>
+                </a>
+                <a href="">
+                    <li>سياسية الخصوصية</li>
+                </a>
+                <a href="#">
+                    <li>الأسئلة الشائعة</li>
+                </a>
+                <a href="#">
+                    <li>الدعم الفني</li>
+                </a>
+
+
+
+
+
+
+
+
+
+
+            </ul>
+        </div>
+
+    </div>
+    <hr class="my-10 border-white/40">
+    <div class="flex flex-col justify-between gap-y-10 lg:flex-row">
+        <div class="flex items-center gap-5 ">
+            <h1 class="text-white">حقوق التأليف والنشر © 2024 سالم العجمي - جميع الحقوق محفوظة | تصميم و تطوير</h1>
+            <x-commun.soitc-logo class="pb-1" />
+        </div>
+        <div class="flex gap-8">
+            <a href="#instagram">
+                <x-icons.instagram class="fill-white" />
+            </a>
+            <a href="#facebook">
+                <x-icons.facebook class="fill-white" />
+            </a>
+            <a href="#twitter">
+                <x-icons.twitter class="fill-white" />
+            </a>
+            <a href="#youtube">
+                <x-icons.youtube class="fill-white" />
+            </a>
+        </div>
+    </div>
+    <svg class=" absolute right-0 -z-[1] -top-1/4 h-80" viewBox="0 0 174 475" fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect width="307.664" height="386.872" rx="20"
+            transform="matrix(-0.707107 -0.707107 -0.707107 0.707107 483.591 209.547)" fill="#C1A661" />
+    </svg>
+    <x-icons.palatte-element class="absolute right-0 bottom-0 -z-[1] h-80 w-fit" />
+</footer>
