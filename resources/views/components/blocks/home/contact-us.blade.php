c889730ef5025da7c85544081a4ffa081f8d717c
KassemHachi <kassem.hachi@outlook.com>
2023-12-19 17:49:13 +0100

    add contact us in block

---
+++ resources/views/components/blocks/home/contact-us.blade.php
@@ -0,0 +1,58 @@
+<section id="contact-us" class=" px-20 bg-[#fbf7f3]">
+    <div class="relative gap-3 mx-auto lg:grid lg:grid-cols-5">
+        <div class="py-16 lg:col-span-2 lg:py-24 ">
+            <div class="max-w-lg mx-auto">
+                <h2 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                    تواصل معنا بطريقة سهلة
+                </h2>
+                <p class="mt-3 text-lg leading-6 text-gray-500">
+                    قم بالتواصل معنا بطريقة سهلة بادخال معلوماتك و كتابة رسالتك. وراح نريد عليك قريبا..
+                </p>
+
+            </div>
+        </div>
+        <div class="px-6 py-16 lg:col-span-3 lg:px-8 lg:py-24 xl:pl-12">
+            <div class="max-w-lg mx-auto lg:max-w-none">
+                <form action="#" method="POST" class="grid grid-cols-2 gap-x-2 gap-y-6">
+                    <div class="col-span-1">
+                        <label for="email" class="leading-8">البريد الإلكتروني</label>
+                        <input type="email" name="email" id="email" autocomplete="email"
+                            class="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            placeholder="salem@mail.com">
+                    </div>
+                    <div class="col-span-1">
+                        <label for="full-name" class="leading-8">الإسم الكامل</label>
+                        <input type="text" name="full-name" id="full-name" autocomplete="name"
+                            class="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            placeholder="Salem Ajemi">
+                    </div>
+                    <div class="col-span-1">
+                        <label for="company" class="leading-8">مؤسسة (اختياري)</label>
+                        <input id="company" name="company" type="text" autocomplete="company"
+                            class="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            placeholder="yourcompany.com">
+                    </div>
+                    <div class="col-span-1">
+                        <label for="phone" class="leading-8">رقم الهاتف</label>
+                        <input type="text" name="phone" id="phone" autocomplete="tel"
+                            class="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            placeholder="+966 123 456 789">
+                    </div>
+                    <div class="col-span-2">
+                        <label for="message" class="leading-8">الرسالة</label>
+                        <textarea id="message" name="message" rows="4"
+                            class="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            placeholder=""></textarea>
+                    </div>
+                    <div>
+                        <button type="submit"
+                            class="flex items-center gap-2 px-3 py-2 text-sm font-semibold leading-6 text-white transition-all duration-150 ease-linear border-2 rounded-lg group bg-salem-dark-blue-500 ">
+                            إرسال
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</section>
