2b5ff64a80f742596e21e5c7326af348fa132eaa
KassemHachi <kassem.hachi@outlook.com>
2023-12-21 19:10:28 +0100

    add error 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title',"سالم عن قرب")
+@use(App\Enums\ItemsNavEnum)
+
+@section('body')
+<x-commun.navbar />
+<x-blocks.simple-page.hero title="الصفحة غير موجودة 404!"
+    imageurl="{{Vite::asset('resources/images/salem-hero-image-2.jpg')}}" />
+
+<article class="min-h-fit  lg:min-h-screen px-10 lg:px-20 py-[3.75rem]">
+
+    <h3 class="text-[25px] font-semibold text-salem-dark-blue-500" data-aos-once="true" data-aos="fade-up"
+        data-aos-delay="0" data-aos-duration="500">
+
+        عذرا لم يتم العثور على الصفحة
+
+    </h3>
+    <p class="text-base text-[#333] mt-4" data-aos-once="true" data-aos="fade-up" data-aos-delay="0"
+        data-aos-duration="500">
+        عذراً، لم يتم العثور على الصفحة التي تبحث عنها. قد يكون هناك خطأ في الرابط أو قد تكون الصفحة غير متاحة حاليًا.
+        يُرجى التحقق من الرابط المدخل أو المحاولة مرة أخرى في وقت لاحق. في حال استمرار المشكلة، يمكنك الاتصال بفريق
+        الدعم الفني للمساعدة. نعتذر عن أي إزعاج قد يسببه هذا الأمر ونشكرك على صبرك وتفهمك.
+    </p>
+</article>
+
+<x-commun.footer />
+
+@endsection
